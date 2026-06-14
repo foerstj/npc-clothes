bbb9c7372a22fb26aa7de7d64a0f723d9f352570
Johannes Förstner <johannes@famfoerstner.de>
2026-06-14 17:05:50 +0200

    Build with plain mod name

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 :: name of map
 set map=npc-clothes-demo
-set res=npc-clothes
+set mod=npc-clothes
 :: name of map, case-sensitive
+set mod_cs=NPC Clothes
 set map_cs=NPC Clothes Demo
 :: tank properties
 set year=2026
@@ -28,6 +29,6 @@ if %errorlevel% neq 0 pause
 
 :: Compile main resource file
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\world\contentdb\templates\%res%" "%tmp%\Bits\world\contentdb\templates\%res%" /S
-"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
+robocopy "%bits%\world\contentdb\templates\%mod%" "%tmp%\Bits\world\contentdb\templates\%mod%" /S
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
