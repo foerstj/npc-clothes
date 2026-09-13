37c8a99702099078e16f3ca21ed787286bb794fc
Johannes Förstner <johannes@famfoerstner.de>
2026-09-13 13:04:51 +0200

    Generate a mart merchant

--- generate.bat
+++ generate.bat
@@ -4,4 +4,7 @@ set bits=%~dp0.
 pushd "%GasPy%"
 venv\Scripts\python -m jinja gaspy\jinja\npc-clothes world\contentdb\templates\npc-clothes --for-each gaspy\jinja\npc-clothes\items.csv --bits "%bits%"
 if %errorlevel% neq 0 pause
+
+venv\Scripts\python -m jinja gaspy\jinja\npc-clothes-mart world\contentdb\templates\npc-clothes-mart --for-all gaspy\jinja\npc-clothes\items.csv --bits "%bits%"
+if %errorlevel% neq 0 pause
 popd
